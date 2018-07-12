3ab9bc1cd2d08189011226b5a7af41692fa940e9
Wongky <32792743+Wongky@users.noreply.github.com>
2018-07-12 16:23:47 +0800

    test crash

--- pos/main.cpp
+++ pos/main.cpp
@@ -99,24 +99,25 @@ void ssend (sstate_ state)
 
 //pos
 const uint8_t COUNT_PER_CM = 200;
-const float CM_PER_COUNT = 1.0/COUNT_PER_CM;  //0.041666666
-const float PI = 22/7.0;
-const float DEG_PER_RAD = 180.0/PI; //1 rad = 180/pi
-const uint8_t WHEEL_DIST = 12;
+//const float CM_PER_COUNT = 1.0/COUNT_PER_CM;  //0.041666666
+//const float PI = 22/7.0;
+//const float DEG_PER_RAD = 180.0/PI; //1 rad = 180/pi
+//const uint8_t WHEEL_DIST = 12;
 const uint8_t Y_CENTER_OFFEST = 12;
 const int16_t CARX = 189/2;
 const int16_t CARY = 120/2 + Y_CENTER_OFFEST;
-const int16_t CARW = 16; //cm
-const int16_t XCOOR_IMAGE_OFFSET = 189/2; //lowest middle of image is (0,0)
+//const int16_t CARW = 16; //cm
+//const int16_t XCOOR_IMAGE_OFFSET = 189/2; //lowest middle of image is (0,0)
 const int16_t O_X_LEFT = 60;
 const int16_t O_X_RIGHT = 150;
 const int16_t O_NX_OFFSET = 5;
 const int16_t O_NX_LEFT = (CARX - (O_X_RIGHT - CARX)) - O_NX_OFFSET;
 const float O_X_LEFT_INC_RATIO = 1.0*(((O_X_RIGHT - CARX)+O_NX_OFFSET)-(CARX- O_X_LEFT))/(CARX- O_X_LEFT);
-const float X_CM_PER_PIX = 0.1; //1pixel = 1mm, x-axis
-const float Y_CM_PER_PIX = 2;
-const int8_t YX_ratio = 20; //20mm/1mm
+//const float X_CM_PER_PIX = 0.1; //1pixel = 1mm, x-axis
+//const float Y_CM_PER_PIX = 2;
+//const int8_t YX_ratio = 20; //20mm/1mm
 
+//approach
 const int16_t A_X_LEFT = 90;
 const int16_t A_X_RIGHT = 110;
 const int16_t A_X_CENTER = 99;
@@ -212,20 +213,15 @@ int main(void)
 //var////////////////////////////////////////
 
 
-
-
-
 	//time
     uint32_t process_time = 0;
     uint32_t avoid_past_time = System::Time();
     uint32_t pid_time = 0;
     uint32_t not_find_time = 0;
     uint32_t finding_time = 0;
+////////////////////////////////////////////
 
-
-    //move dis
-
-/////////////////////////////////////////
+//set//////////////////////////////////////
 
     run = false;
     chasing_speed = 200;
@@ -238,7 +234,7 @@ int main(void)
 
 	display_bMeter();
 	changeSpeedTime = System::Time();
-
+///////////////////////////////////////////////
 
 	while (true){
 		if (tick != System::Time()) {
@@ -257,6 +253,7 @@ int main(void)
 				if(accCount ==true && firstAcc==true)
 				{
 					firstAcc=false;
+					continue;
 				}
 				else if(accCount ==true && firstAcc==false)
 				{
@@ -316,7 +313,7 @@ int main(void)
 				SetPower(0,1);
 				aaction = stops;
 			}
-///////////////////////////////////
+//pid end/////////////////////////////////
 
 //process///////////////////////////
 			if (tick - process_time >= 29) {
@@ -334,11 +331,31 @@ int main(void)
 				{
 					if(aaction == sstate_::avoids)
 					{
-						moveCount(-30, sstate_::backwards, sstate_::forwards);
+						moveCount(-30, sstate_::backwards, sstate_::forwards); //avoid again if seen obstacle
 					}
 					else moveCount(-30, sstate_::backwards, aaction);
 					aaction = backwards;
 					actionTarget(aaction);
+					ssend(aaction);
+					continue;
+				}
+				else if(run==true && aaction==sstate_::backwards && tick-changeSpeedTime>=200 && (
+						(!(abs(L_pid->getTarget())<20) && (abs(L_count)<20))
+						|| (!(abs(R_pid->getTarget())<20) && (abs(R_count)<20))
+					))
+				{
+					if(abs(L_count)>abs(R_count))
+					{
+						moveCount(50, sstate_::turnLefts, sstate_::forwards);
+						aaction = turnLefts;
+					}
+					else
+					{
+						moveCount(50, sstate_::turnRights, sstate_::forwards);
+						aaction = turnRights;
+					}
+					actionTarget(aaction);
+					finding_time = 0; //trigger rotations in not seen condition check
 					ssend(aaction);
 					continue;
 				}
@@ -391,6 +408,7 @@ int main(void)
 					if ( x > O_X_LEFT && x < O_X_RIGHT)
 					{
 
+						// too close
 						if (y> 60){
 							if(aaction!=backwards)
 							{
@@ -411,38 +429,31 @@ int main(void)
 						else
 						{
 							setAnglePower(BeaconAvoidAngleCalculate(x, y), tick, pid_time);
-							avoid_past_time = System::Time();
+							//avoid_past_time = System::Time();
 							aaction = avoids;
 							changeSpeedTime = System::Time();
 						}
 						ssend(aaction);
 						continue;
 					}
-//					else if(avoid_past_time-tick>=100)
-//					{
-//						if(aaction == avoids)
-//						{
-//							//forward some distance before rotation
-//							moveCount(30, sstate_::forwards, sstate_::rotations);
-//							aaction = forwards;
-//							actionTarget(aaction);
-//							//pid(tick, pid_time);
-//						}
-//					}
 				}
 				else if(tick - o_target.received_time >= 200)
 				{
 					led0->SetEnable(false);
+
+					//aviod for some time
 					if(aaction == avoids)
 					{
 						//forward some distance before rotation
-						moveCount(40, sstate_::forwards, sstate_::rotations);
+						//moveCount(40, sstate_::forwards, sstate_::rotations);
+						moveCount(40, sstate_::forwards, sstate_::forwards);
 						aaction = forwards;
 						actionTarget(aaction);
+						finding_time = 0; //trigger rotations in not seen condition check
 						//pid(tick, pid_time);
 					}
 				}
-////////////////////////////////
+//otarget end//////////////////////////////
 
 
 //irtarget2////////////////////////
@@ -450,6 +461,15 @@ int main(void)
 				{
 					uint16_t x = ir_target2.target->center.first;
 					uint16_t y = ir_target2.target->center.second;
+
+					//ir cam
+					not_find_time = 0;
+					finding_time = 0;
+
+					if (!seen) {
+						seen = true;
+						Dir_pid->reset();
+					}
 
 					led1->SetEnable(true);
 					if(printLCD)
@@ -460,6 +480,7 @@ int main(void)
 						writer->WriteString(temp);
 					}
 
+					//too close, circle
 //					if (y>= 20 && x>=A_X_LEFT && x<=A_X_RIGHT){
 //
 //					}
@@ -476,7 +497,7 @@ int main(void)
 //				{
 //					led1->SetEnable(false);
 //				}
-///////////////////////////////////
+//ir target 2 end/////////////////////////////////
 
 //ir target/////////////////////////
 				if (ir_target != NULL) {	//target find
@@ -495,7 +516,9 @@ int main(void)
 						target_x = 320;
 					if (aaction == rotations
 							&& ir_target->center.first > target_x)
-						aaction = keeps;
+					{
+						//aaction = keeps;
+					}
 					else
 					{
 						aaction = chases;
@@ -505,7 +528,7 @@ int main(void)
 				else if (seen) { //target not find but have seen target before
 					if (not_find_time == 0) {
 						not_find_time = tick;
-						aaction = keeps;
+						//aaction = keeps;
 					} else if (tick - not_find_time > 400) { //target lost for more than 400 ms
 						led1->SetEnable(0);
 						seen = false;
@@ -518,39 +541,40 @@ int main(void)
 
 					}
 				}
-				else if (accCount==true) {
+				else if (accCount==true) { //forward move count
 					finding_time = tick;
 				}
 				else { //target not find and have not seen target before
 					led1->SetEnable(0);
-					if (finding_time == 0 && aaction!=forwards)
+					if (finding_time == 0 && aaction!=sstate_::forwards)
 					{
 						moveCount(30, sstate_::forwards, sstate_::rotations);
 						aaction = forwards;
 						actionTarget(aaction);
 						finding_time = tick;
 					}
-					else if(finding_time != 0 && aaction == rotations && tick-finding_time>2040)//rotate 2000ms
+					else if(finding_time ==0 && aaction== sstate_::forwards)
+					{
+						aaction = rotations;
+						actionTarget(aaction);
+						finding_time = tick;
+					}
+					else if(finding_time != 0 && aaction == sstate_::rotations && tick-finding_time>2040)//rotate 2000ms
 					{
 						finding_time = 0;
 					}
-					else if(finding_time != 0 && aaction != rotations)
+					else if(finding_time != 0 && aaction != sstate_::rotations) //unknown
 					{
 						finding_time = 0;
 					}
 
 				}
-////////////////////////////////////
+//ir target end//////////////////////////////////
 
 
 				ssend(aaction);
 			}
-/////////////////////////////////////
-//			if(tick - avoid_past_time >=15000){
-//				SetPower(0, 0);
-//				SetPower(0, 1);
-//				run = false;
-//			}
+//process end///////////////////////////////////
 		}
 
 
@@ -836,55 +860,65 @@ void pid(const uint32_t& tick, uint32_t& pid_time)
 	uint32_t time_diff = tick - pid_time;
 	encoder1->Update();
 	encoder2->Update();
-	int32_t lc = encoder1->GetCount();
-	int32_t rc = encoder2->GetCount();
+	L_count = encoder1->GetCount() * 50 / (int) time_diff;
+	R_count = encoder2->GetCount() * 50 / (int) time_diff;
+	SetPower(L_pid->output(L_count), 0);
+	SetPower(R_pid->output(-R_count), 1);
+	pid_time = System::Time();
 
 	if(accCount ==true && firstAcc==true)
 	{
 		firstAcc=false;
+		return;
 	}
 	else if(accCount ==true && firstAcc==false)
 	{
-		aCountL += lc;
-		aCountR += rc;
+		aCountL += L_count;
+		aCountR += R_count;
 	}
 
 	//move dis
-	if(accCount == true)
+	if(accCount == true && firstAcc==false)
 	{
+//					{
+//						char temp[20] = { };
+//						sprintf(temp, "al=%d ar=%d", aCountL,aCountR);
+//						lcd->SetRegion(Lcd::Rect(0, 20, 128, 160));
+//						writer->WriteString(temp);
+//						sprintf(temp, "dis=%d", accDis);
+//						lcd->SetRegion(Lcd::Rect(0, 40, 128, 160));
+//						writer->WriteString(temp);
+//					}
 		if(accAction == aaction)
 		{
+			ssend(accAction);
 			//move forward
 			if(accDis>0)
 			{
 				if(aCountL>=accDis || aCountR<= - (accDis))
 				{
 					accCount = false;
+					aaction = actionAfterMove;
 					actionTarget(actionAfterMove);
 				}
 			}
 			//move backward
 			else //(accDis<0)
 			{
-				if(aCountR>=accDis || aCountL<= - (accDis))
+				if(aCountL<=accDis || aCountR>= abs (accDis))
 				{
 					accCount = false;
+					aaction = actionAfterMove;
 					actionTarget(actionAfterMove);
 				}
 			}
 		}
-		// action is changed, move is outdated
+		 //action is changed, move is outdated
 		else
 		{
 			accCount = false;
 		}
 	}
-
-	L_count = lc * 50 / (int) time_diff;
-	R_count = rc * 50 / (int) time_diff;
-	SetPower(L_pid->output(L_count), 0);
-	SetPower(R_pid->output(-R_count), 1);
-	pid_time = System::Time();
 }
 
 void actionTarget(const sstate_& taction)
