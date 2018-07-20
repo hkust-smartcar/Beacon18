29057d49a839758662ade5a0ce5e91cfa269b8c4
Wongky <32792743+Wongky@users.noreply.github.com>
2018-07-20 22:37:42 +0800

    acc, count

--- pos/inc/pid.h
+++ pos/inc/pid.h
@@ -23,18 +23,29 @@ public:
 	int32_t min;
 	//assume errorIgnore always positive
 	int32_t errorIgnore;
+	int32_t errorAcc;
+	uint16_t numOfLower;
+	int32_t errorAccept;
 
 	PID() :
-			kP(0), kI(0), kD(0), errorSumBound(0), max(0), min(0), preError(0), errorSum(
-					0), last_time(0), target(0), errorIgnore(70) {
+			kP(0), kI(0), kD(0), errorSumBound(0), max(0), min(0), preError(0), errorSum(0),
+			last_time(0), target(0), errorIgnore(70),errorAcc(0),isAcc(false),numOfLower(0),errorAccept(0), isCount(false),changeTime(0) {
 	}
 
 	PID(const float& p, const float& i, const float& d, int m_max, int m_min) :
-			kP(p), kI(i), kD(d), errorSumBound(0), max(m_max), min(m_min), preError(
-					0), errorSum(0), last_time(0), target(0), errorIgnore(70) {
+			kP(p), kI(i), kD(d), errorSumBound(0), max(m_max), min(m_min), preError(0),
+			errorSum(0), last_time(0), target(0), errorIgnore(70),errorAcc(0),isAcc(false),numOfLower(0),errorAccept(0), isCount(false), changeTime(0){
 	}
 	void settarget(int32_t t) {
-		target = t;
+		if(target!=t)
+		{
+			target = t;
+			errorAccept = abs(target * 0.4);
+			changeTime = System::Time();
+			isAcc = true;
+
+		}
+
 	}
 	int32_t getTarget() {
 		return target;
@@ -42,8 +53,48 @@ public:
 	int32_t output(const int32_t& current) {
 		uint32_t time_diff = System::Time() - last_time;
 		int32_t error = target - current;
+
+		if(isAcc && System::Time() -changeTime>200)
+		{
+			isAcc = false;
+			errorAcc = 0;
+		}
+
+
 		accuError(error);
-		float ki = errorSum * kI /** time_diff*/;
+
+		if(isCount)
+		{
+			if(target>0)
+			{
+				if(current<target && abs(error)>errorAccept)
+				{
+					numOfLower ++;
+				}
+				else
+				{
+					numOfLower=0;
+				}
+			}
+			else if(target<0)
+			{
+				if(target<current && abs(error)>errorAccept)
+				{
+					numOfLower++;
+				}
+				else
+				{
+					numOfLower=0;
+				}
+			}
+		}
+
+		float ki = 0;
+		if(isAcc)
+		{
+			ki = (errorSum + errorAcc) * kI;
+		}
+		else ki = errorSum * kI /** time_diff*/;
 		float kd = (error - preError) * kD /*/ time_diff*/;
 		float kp = error * kP;
 		int32_t out = (int32_t) (kp + ki + kd);
@@ -66,12 +117,77 @@ public:
 	void reset() {
 		errorSum = 0;
 		preError = 0;
+		isAcc = false;
+		isCount =false;
+		errorAcc = 0;
+		numOfLower= 0;
 		last_time = System::Time();
+		changeTime = 0;
 	}
 
-	int32_t getErrorSum()
+//	int32_t getErrorSum()
+//	{
+//		return errorSum;
+//	}
+
+	uint16_t getNumError()
 	{
-		return errorSum;
+		return numOfLower;
+	}
+
+//	int32_t getErrorAcc()
+//	{
+//		return errorAcc;
+//	}
+
+//	int32_t getIsAcc()
+//	{
+//		return isAcc;
+//	}
+//
+//	void setIsAcc(bool f)
+//	{
+//		if(f==true && isAcc==false)
+//		{
+//			isAcc = f;
+//			errorAcc=0;
+//		}
+//		else if(f==false && isAcc==true)
+//		{
+//			isAcc = f;
+//			errorAcc=0;
+//		}
+//
+//	}
+
+	void setIsCount(bool f)
+	{
+		if(f==true && isCount==false)
+		{
+			isCount = f;
+			numOfLower= 0;
+		}
+		else if(f==false && isCount==true)
+		{
+			isCount = f;
+			numOfLower= 0;
+		}
+	}
+
+	void resetAcc()
+	{
+		errorAcc=0;
+
+	}
+
+
+	bool getErrorFull()
+	{
+		if(abs(errorAcc) >= 5000)
+		{
+			return true;
+		}
+		return false;
 	}
 
 private:
@@ -79,6 +195,10 @@ private:
 	int32_t errorSum;
 	int32_t last_time;
 	int32_t target;
+	int32_t changeTime;
+	bool isAcc;
+	bool isCount;
+
 	void accuError(const int32_t& e) {
 		//assume errorSumBound always positive
 		if ((errorSum + e) > errorSumBound) {
@@ -87,6 +207,17 @@ private:
 			errorSum = -errorSumBound;
 		} else {
 			errorSum += e;
+		}
+
+		if(isAcc)
+		{
+			if ((errorAcc + e) > errorSumBound) {
+				errorAcc = errorSumBound;
+			} else if ((errorSum + e) < -errorSumBound) {
+				errorAcc = -errorSumBound;
+			} else {
+				errorAcc += e;
+			}
 		}
 	}
 
