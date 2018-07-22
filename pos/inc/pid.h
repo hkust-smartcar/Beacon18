13e26555286b251e5fcbbd7e591c9c6b0213bac6
Wongky <32792743+Wongky@users.noreply.github.com>
2018-07-22 10:42:40 +0800

    Update pid.h

--- pos/inc/pid.h
+++ pos/inc/pid.h
@@ -19,33 +19,54 @@ public:
 
 	//assume errorSumBound always positive
 	int32_t errorSumBound;
+	int32_t max;
+	int32_t min;
 
 	PID() :
-			kP(0), kI(0), kD(0), errorSumBound(0), preError(0), errorSum(0) {
+			kP(0), kI(0), kD(0), errorSumBound(0), max(0), min(0), preError(0), errorSum(
+					0), last_time(0), target(0) {
 	}
 
-	PID(const float& p, const float& i, const float& d) :
-			kP(p), kI(i), kD(d), errorSumBound(0), preError(0), errorSum(0) {
+	PID(const float& p, const float& i, const float& d, int m_max, int m_min) :
+			kP(p), kI(i), kD(d), errorSumBound(0), max(m_max), min(m_min), preError(
+					0), errorSum(0), last_time(0), target(0) {
 	}
-
-	int32_t output(const int32_t& target, const int32_t& current) {
+	void settarget(int t) {
+		target = t;
+	}
+	int32_t getTarget() {
+		return target;
+	}
+	int32_t output(const int32_t& current) {
+		uint32_t time_diff = System::Time() - last_time;
 		int32_t error = target - current;
 		accuError(error);
-		int32_t out = (int32_t)(
-				error * kP + errorSum * kI + (error - preError) * kD);
+		float ki = errorSum * kI /** time_diff*/;
+		float kd = (error - preError) * kD /*/ time_diff*/;
+		float kp = error * kP;
+		int32_t out = (int32_t) (kp + ki + kd);
+		if(max != 0 && min != 0){
+			if(out < min)
+				out = min;
+			else if(out > max)
+				out = max;
+		}
 		preError = error;
+		last_time = System::Time();
 		return out;
 	}
 
 	void reset() {
 		errorSum = 0;
 		preError = 0;
-		errorSum = 0;
+		last_time = System::Time();
 	}
 
 private:
 	int32_t preError;
 	int32_t errorSum;
+	int32_t last_time;
+	int32_t target;
 	void accuError(const int32_t& e) {
 		//assume errorSumBound always positive
 		if ((errorSum + e) > errorSumBound) {
@@ -57,5 +78,6 @@ private:
 		}
 	}
 };
+
 
 #endif /* PID_H */
