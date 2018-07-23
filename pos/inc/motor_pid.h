56a6a4090c5e8a7b7d4b29fa12eb12cc270df93f
Wongky <32792743+Wongky@users.noreply.github.com>
2018-07-23 16:27:38 +0800

    many change
    aactiontarget funtion: same action will have new change speed time;; 
    => change time when differnt target
    
    
    every chases: reset count error num => cant accumalte error num when null turn
    =>first chases time>200 to accumulte error
    
    rotation and search 500ms change speed time';;
    => add acc error
    
    avoid and approach
    =>acctiontarget break

--- pos/inc/motor_pid.h
+++ pos/inc/motor_pid.h
@@ -23,27 +23,24 @@ public:
 	int32_t min;
 	//assume errorIgnore always positive
 	int32_t errorIgnore;
-	int32_t errorAcc;
+	float errorAcceptRate;
 	uint16_t numOfLower;
-	int32_t errorAccept;
 
 	Motor_PID() :
 			kP(0), kI(0), kD(0), errorSumBound(0), max(0), min(0), preError(0), errorSum(0),
-			last_time(0), target(0), errorIgnore(70),errorAcc(0),isAcc(false),numOfLower(0),errorAccept(0), isCount(false),changeTime(0) {
+			target(0), errorIgnore(70),errorAcceptRate(0.4),errorAcc(0),isAcc(false),numOfLower(0),errorAccept(0), isCount(false) {
 	}
 
 	Motor_PID(const float& p, const float& i, const float& d, int m_max, int m_min) :
 			kP(p), kI(i), kD(d), errorSumBound(0), max(m_max), min(m_min), preError(0),
-			errorSum(0), last_time(0), target(0), errorIgnore(70),errorAcc(0),isAcc(false),numOfLower(0),errorAccept(0), isCount(false), changeTime(0){
+			errorSum(0),target(0), errorIgnore(70),errorAcceptRate(0.4),errorAcc(0),isAcc(false),numOfLower(0),errorAccept(0), isCount(false){
 	}
-	void settarget(int32_t t) {
+	void settarget(const int32_t& t) {
 		if(target!=t)
 		{
 			target = t;
 			errorAccept = abs(target * 0.4);
-			changeTime = System::Time();
 			isAcc = true;
-
 		}
 
 	}
@@ -51,15 +48,8 @@ public:
 		return target;
 	}
 	int32_t output(const int32_t& current) {
-		uint32_t time_diff = System::Time() - last_time;
+		//uint32_t time_diff = System::Time() - last_time;
 		int32_t error = target - current;
-
-//		if(isAcc && System::Time() -changeTime>200)
-//		{
-//			isAcc = false;
-//			errorAcc = 0;
-//		}
-
 
 		accuError(error);
 
@@ -92,7 +82,13 @@ public:
 		float ki = 0;
 		if(isAcc)
 		{
-			ki = (errorSum + errorAcc) * kI;
+			if ((errorAcc + errorSum) > errorSumBound) {
+				ki = errorSumBound * kI;
+			} else if ((errorSum + errorSum) < -errorSumBound) {
+				ki = -errorSumBound * kI;
+			} else {
+				ki = (errorSum + errorAcc) * kI;
+			}
 		}
 		else ki = errorSum * kI /** time_diff*/;
 		float kd = (error - preError) * kD /*/ time_diff*/;
@@ -105,7 +101,6 @@ public:
 				out = max;
 		}
 		preError = error;
-		last_time = System::Time();
 		//assume errorIgnore always positive
 		if(!isAcc && abs(error)>errorIgnore)
 		{
@@ -121,31 +116,16 @@ public:
 		isCount =false;
 		errorAcc = 0;
 		numOfLower= 0;
-		last_time = System::Time();
-		changeTime = 0;
 	}
 
-//	int32_t getErrorSum()
-//	{
-//		return errorSum;
-//	}
 
-	uint16_t getNumError()
-	{
-		return numOfLower;
-	}
-
-//	int32_t getErrorAcc()
-//	{
-//		return errorAcc;
-//	}
-
+//acc
 	int32_t getIsAcc()
 	{
 		return isAcc;
 	}
 
-	void setIsAcc(bool f)
+	void setIsAcc(const bool& f)
 	{
 		if(f==true && isAcc==false)
 		{
@@ -160,13 +140,31 @@ public:
 
 	}
 
+	void addErrorAcc(const int32_t n)
+	{
+		if ((errorAcc + n) > errorSumBound) {
+			errorAcc = errorSumBound;
+		} else if ((errorSum + n) < -errorSumBound) {
+			errorAcc = -errorSumBound;
+		} else {
+			errorAcc += n;
+		}
+	}
+
 	void resetAcc()
 	{
 		isAcc = false;
 		errorAcc=0;
 	}
 
-	void setIsCount(bool f)
+
+//num
+	uint16_t getNumError()
+	{
+		return numOfLower;
+	}
+
+	void setIsCount(const bool& f)
 	{
 		if(f==true && isCount==false)
 		{
@@ -180,22 +178,12 @@ public:
 		}
 	}
 
-
-	bool getErrorFull()
-	{
-		if(abs(errorAcc) >= 5000)
-		{
-			return true;
-		}
-		return false;
-	}
-
 private:
 	int32_t preError;
 	int32_t errorSum;
-	int32_t last_time;
 	int32_t target;
-	int32_t changeTime;
+	int32_t errorAcc;
+	int32_t errorAccept;
 	bool isAcc;
 	bool isCount;
 
