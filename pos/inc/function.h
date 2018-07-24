f8543a44d1b56b9ce0f47f431f59225b6f6928e9
Wongky <32792743+Wongky@users.noreply.github.com>
2018-07-24 20:57:54 +0800

    first forward clockwise

--- pos/inc/function.h
+++ pos/inc/function.h
@@ -160,6 +160,8 @@ bool bt_listener(const Byte *data, const size_t size) {
 	if (data[0] == 's') {
 		run = true;
 		firstRun = true;
+		L_motor->SetClockwise(false);
+		R_motor->SetClockwise(true);
 		changeSpeedTime = System::Time();
 		chases_crash_time = 0;
 		led0->SetEnable(1);
